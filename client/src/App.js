8d6c896e7d691a3af35ed54749d0c4f87f2ba013
Thai <pumpo6969@gmail.com>
2024-02-23 10:37:58 +0700

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Access from "./pages/Access";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/*" element={<Access />} />
+          <Route path="/" element={<Home />} />
+          
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./home.css"
+
+const Home = () => {
+    return (
+        <div>xin loi</div>
+    )
+}
+
+export default Home
