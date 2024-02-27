548726f1142ac5a1725367231dbbfdf023951dc8
Thai <pumpo6969@gmail.com>
2024-02-27 18:11:08 +0700

    client

---
+++ client/src/components/propertyList/propertyList.jsx
@@ -0,0 +1,59 @@
+import "./propertyList.css"
+
+const PropertyList = () => {
+    return (
+        <div className="pList">
+        <div className="pListItem">
+            <img src="/IMG/Home/Dalat2.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Da lat</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="/IMG/Home/Vungtau.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Vung tau</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="/IMG/Home/Danang.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Da nang</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="/IMG/Home/Nhatrang.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Nha trang</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+        <div className="pListItem" >
+            <img src="/IMG/Home/HoChiMinh.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Ho Chi Minh City</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="/IMG/Home/Hanoi.jpg" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Ha Noi</h1>
+                <h2>1111 properties</h2>
+            </div>
+        </div>
+
+      
+        </div>
+    )
+}
+
+export default PropertyList
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Navbar from "../../components/navbar/navBar"
 import Header from "../../components/header/Header"
 import "./home.css"
-
+import Featured from "../../components/featured/Featured"
+import PropertyList from "../../components/propertyList/propertyList"
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Header />
+            <div className="homeContainer">
+                <div>
+                <h1 className="homeTitle">Trending destinations</h1>
+                <p className="homeDecrip">Most popular choices for travellers from Vietnam</p>
+                </div>
+                <Featured />
+                <div>
+                <h1 className="homeTitle">Explore Vietnam</h1>
+                <p className="homeDecrip">These popular destinations have a lot to offer</p>
+                </div>
+                <PropertyList />
+            </div>
         </div>
     )
 }
