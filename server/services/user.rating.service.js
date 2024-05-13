e1874c92916e81387fb57820c70cb5b02f52a3fa
Nguyê~n Minh Thành <middmoon.68@gmail.com>
2024-05-14 06:25:26 +0700

    update api

---
+++ server/services/user.rating.service.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const { NotFoundError } = require("../core/error.response");
+const db = require("../models");
+
+class UserRatingService {
+  static async createRating(userId) {}
+}
+
+module.exports = UserRatingService;
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -67,10 +67,6 @@ class UserService {
       };
     }
   }
-
-  static async getHotelList() {
-    const hotelList = await db.Hotel.find;
-  }
 }
 
 module.exports = UserService;
